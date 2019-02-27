85467d3cb3e18040410b24c0a961cc15d6b216d9
Ruben <rubenreinosobarco@gmail.com>
2019-02-27 11:47:15 +0100

    Initial Commit

---
+++ 04-Piano/Extensions.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  dados
+//
+//  Created by Arig Ahissami Yordi on 25/2/19.
+//  Copyright © 2019 Arig Ahissami Yordi. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable extension UIButton {
+    
+    @IBInspectable var borderWidth: CGFloat{
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    
+    @IBInspectable var cornerRadius : CGFloat{
+        set{
+            layer.cornerRadius = newValue
+        }
+        get{
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor : UIColor? {
+        set{
+            guard let uiColor = newValue else {return}
+            layer.borderColor = uiColor.cgColor
+        }
+        get{
+            guard let color = layer.borderColor else {return nil}
+            return UIColor(cgColor: color)
+        }
+    }
+    
+}
